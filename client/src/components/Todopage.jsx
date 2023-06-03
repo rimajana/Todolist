4a0e612610a61e36d3c057e6a76afe9d3758028a
Rima Jana <66671637+rimajana@users.noreply.github.com>
2023-06-04 00:00:32 +0530

    Update Todopage.jsx

--- client/src/components/Todopage.jsx
+++ client/src/components/Todopage.jsx
@@ -4,7 +4,7 @@ import Todocard from "./Todocard";
 function Todopage({todos,setTodos}) {
   return (
     <div className="twin">
-      {console.log("todos",todos)}
+//       {console.log("todos",todos)}
       {todos.map((ele)=>{
         if(ele.Description.length !=0) 
         return (<Todocard ele={ele} listt={todos} setlists={setTodos}/>);
